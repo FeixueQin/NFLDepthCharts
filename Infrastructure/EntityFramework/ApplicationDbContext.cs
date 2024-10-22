38fb6204e9cbc210c808c9fb494aa920ab144c50
FeixueQin <fxqin16@gmail.com>
2024-10-22 19:39:18 +1100

    [NDS-1] Create Solution And Projects

---
+++ Infrastructure/EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define DbSets for your entities here, for example:
+        public DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
